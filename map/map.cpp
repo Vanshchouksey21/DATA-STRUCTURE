7f53c4e1e650a62d58529217dd0526700c4211f8
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-03-20 18:44:05 +0530

    occurencs count in a string

--- map/map.cpp
+++ map/map.cpp
@@ -13,12 +13,38 @@
 #include<map>
 using namespace std ; 
 int main (){
-    map<int , string>student {{101 , "amit" } , {102 , "vansh"} , {21 , "vaibhav"}};
+
+
+    // map<int , string>student {{101 , "amit" } , {102 , "vansh"} , {21 , "vaibhav"}};
+
+
+
+// display type - 1
     // for( auto p :student){
     //     cout<<p.first<<p.second<<"\n";
     // }
-    for(auto p = student.begin() ; p != student.end() ;p++ ){
-        cout<<p->first<<" "<<p->second <<"\n";
+
+
+//  dispaly type - 2 
+    // for(auto p = student.begin() ; p != student.end() ;p++ ) //P=p+1 use nhi kr sakte , because it is not contigeous memory allocation !!!!!!
+    // {
+    //     cout<<p->first<<" "<<p->second <<"\n";
+    // }
+
+
+
+
+
+    map <int , string> abc ;
+    abc.insert({109 , "vansh"});
+    abc.insert({101 , "sumit"});
+    abc.insert({107 , "amit"});
+    abc.insert({106 , "ans"});
+  
+    cout << "Student Map (Sorted by Key):\n";
+    for ( auto p : abc) {
+        cout << "Roll No: " << p.first << ", Name: " << p.second << "\n";
     }
+    
 
 }
