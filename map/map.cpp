6bb2c17a589098ada15b83af9d42ca50fd43bd40
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-03-20 17:53:52 +0530

    map concept

---
+++ map/map.cpp
@@ -0,0 +1,9 @@
+// it is an associate container 
+// it contains key and value 
+// it is sorted by default 
+// key is always unique 
+// self balance  BST or red black tree maintain lexicography order 
+// there are mainly three typres of map :
+//   # ordered map  ( sorted ) (RED BLACK TREE)
+//  # unordered map ( unsorted ) (HASING TECHNIQUE )
+//  # multimap  (dulicate values all allowed )
