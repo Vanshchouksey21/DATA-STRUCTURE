473176c5d443fcd81aaa1e956f9926f52353880b
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-03-24 11:17:59 +0530

    vector

--- vector/vns.cpp
+++ vector/vns.cpp
@@ -1,11 +1,13 @@
 
 
 #include<iostream>
-#include<array>
+#include<vector>
+#include<algorithm>
+
 using namespace std ; 
 int main()
 {
-    array <int , 5> arr{1,8,69,4,4};
+    vector <int > arr{1,8,69,4,4};
     sort(arr.begin(),arr.end());
 
    int size =arr.size();
