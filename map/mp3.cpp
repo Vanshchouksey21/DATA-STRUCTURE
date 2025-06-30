a892a9a53d3dd59b5c3631283393e812ed86a24d
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-06-30 18:31:29 +0530

    dsa

--- map/mp3.cpp
+++ map/mp3.cpp
@@ -5,7 +5,6 @@ int main (){
     map<int , string>mp ;
     int n ;
     cout<<"How many records do you want to enter :" ;
-    cout<<"How many records do you want to enter :" ;
     cin>>n;
     for(int i = 0 ; i < n ; ++i){
         int roll ; 
